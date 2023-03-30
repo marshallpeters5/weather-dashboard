214cec51c3dbb5c348463020eba4c0f9414dd643
Marshall <marshall.peters01@gmail.com>
2023-03-29 21:58:05 -0400

    added more js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,15 @@
-var APIKey = '9c94824efa946f7fbfd1c97e28156fbb'
+function weatherSearch (city) {
+var key = '9c94824efa946f7fbfd1c97e28156fbb'
 var city;
-
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=' + APIKey;
 fetch(queryURL)
+.then(function(resp) { return resp.json() })
+.then(function(data) {
+  console.log(data);
+})
+.catch(function() {
+});
+}
+window.onload = function(){
+    weatherSearch('Orlando');
+}
