9472f134f72696ac3035ddb1a0cb46d051d45f5e
Marshall <marshall.peters01@gmail.com>
2023-03-29 21:58:28 -0400

    formatting

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-function weatherSearch (city) {
-var key = '9c94824efa946f7fbfd1c97e28156fbb'
-var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=' + APIKey;
-fetch(queryURL)
-.then(function(resp) { return resp.json() })
-.then(function(data) {
-  console.log(data);
-})
-.catch(function() {
-});
+function weatherSearch(city) {
+    var key = '9c94824efa946f7fbfd1c97e28156fbb'
+    var city;
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=' + APIKey;
+    fetch(queryURL)
+        .then(function (resp) { return resp.json() })
+        .then(function (data) {
+            console.log(data);
+        })
+        .catch(function () {
+        });
 }
-window.onload = function(){
+window.onload = function () {
     weatherSearch('Orlando');
 }
