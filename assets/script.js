678ddaac1db651fc79a2e168831aae714decaea8
Marshall <marshall.peters01@gmail.com>
2023-04-03 13:18:44 -0400

    added a new input and an event listener

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
-function weatherSearch(city) {
-    var city = 
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.val + '&appid=9c94824efa946f7fbfd1c97e28156fbb';
+function weatherSearch() {
+    var cityInput = document.getElementById('city-input')
+    console.log(cityInput.value)
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + '&appid=9c94824efa946f7fbfd1c97e28156fbb&units=imperial';
     fetch(queryURL)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
@@ -12,3 +13,5 @@ function weatherSearch(city) {
 window.onload = function () {
     weatherSearch();
 }
+
+document.querySelector(".add-city").addEventListener("click", weatherSearch);
