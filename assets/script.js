17da86ad6bc1ac34ccf5385c4e165eb82a7794fb
Marshall <marshall.peters01@gmail.com>
2023-04-03 17:11:45 -0400

    currently working, needs an expanding list

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 var weatherCardEl = document.querySelector('#weather-card')
+
 function weatherSearch() {
     var cityInput = document.getElementById('city-input')
-    var localCityList = []
+        localStorage.setItem("city", cityInput.value);
+    document.getElementById("city-input").innerHTML = localStorage.getItem("city");
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + cityInput.value + '&appid=9c94824efa946f7fbfd1c97e28156fbb&units=imperial';
     fetch(queryURL)
         .then(function (resp) { return resp.json() })
@@ -11,20 +13,23 @@ function weatherSearch() {
         .catch(function () {
         });
 }
-window.onload = function () {
-    weatherSearch();
+function renderSearchHistory() {
+    const newLi = document.createElement("li")
+    const newCity = document.createTextNode(cityInput.value)
+    newLi.appendChild(newCity)
+    const searchHistory = document.getElementById("search-history")
+    document.body.insertBefore(newLi, searchHistory)
 }
-
 function renderWeatherCards(data) {
     for (let index = 0; index < data.list.length; index = index + 8) {
         var date = new Date(data.list[index].dt * 1000)
         var currentDate = date.toLocaleDateString()
         var html =
-        `<div class="card col-2 card-item-padding">
+            `<div class="card col-2 card-item-padding">
             <p class="card-text">Weather Status:</p>
             <img src="https://openweathermap.org/img/w/${data.list[index].weather[0].icon}.png" class="card-img-top" id="icon-image" alt="image placeholder">
             <div class="card-body">
-                <h5 class="card-title">${currentDate}</h5>
+                <h5 class="card-title"> Date: ${currentDate}</h5>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">Temperature: ${data.list[index].main.temp} °F</li>
                     <li class="list-group-item">Humidity: ${data.list[index].main.humidity}%</li>
@@ -35,9 +40,7 @@ function renderWeatherCards(data) {
         </div>`
         weatherCardEl.insertAdjacentHTML('beforeend', html)
     }
-    console.log(data)
-
 }
 
 
-document.querySelector(".add-city").addEventListener("click", weatherSearch);
+document.querySelector(".add-city-btn").addEventListener("click", weatherSearch);
