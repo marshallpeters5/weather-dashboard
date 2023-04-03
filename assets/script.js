4676f477cb1430eba2e3336096b9f78bb1d767ca
Marshall <marshall.peters01@gmail.com>
2023-04-03 12:57:42 -0400

    added bootstrap and some other tools

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,6 @@
 function weatherSearch(city) {
-    var key = '9c94824efa946f7fbfd1c97e28156fbb'
-    var city;
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=' + APIKey;
+    var city = 
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city.val + '&appid=9c94824efa946f7fbfd1c97e28156fbb';
     fetch(queryURL)
         .then(function (resp) { return resp.json() })
         .then(function (data) {
